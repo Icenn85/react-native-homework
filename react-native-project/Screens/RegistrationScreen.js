38bed72f4251f6525453de414020d70ddad8d300
Anastasiia Yavtushenko <Icenn85@gmail.com>
2023-01-30 21:48:21 +0200

    add markup to registration

--- react-native-project/Screens/RegistrationScreen.js
+++ react-native-project/Screens/RegistrationScreen.js
@@ -3,8 +3,11 @@ import {
   ImageBackground,
   StyleSheet,
   View,
-  KeyboardAvoidingView,
-  Dimensions,
+  Text,
+  TouchableOpacity,
+  Image,
+  // KeyboardAvoidingView,
+  // Dimensions,
 } from "react-native";
 
 export default function RegistrationScreen() {
@@ -13,9 +16,19 @@ export default function RegistrationScreen() {
   return (
     <View style={styles.container}>
       <ImageBackground source={image} style={styles.image}>
-
-    
-    
+        <View style={styles.formBg}>
+          <View style={styles.avatar}>
+            <TouchableOpacity>
+              <Image
+                style={styles.addBtn}
+                source={require("../assets/add.png")}
+              />
+            </TouchableOpacity>
+          </View>
+          <View>
+            <Text style={styles.title}>Регистрация</Text>
+          </View>
+        </View>
       </ImageBackground>
     </View>
   );
@@ -28,8 +41,38 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     resizeMode: "cover",
-    justifyContent: "flex-end",
+    justifyContent: "center",
     alignItems: "center",
   },
-  
+  formBg: {
+    position: "absolute",
+    top: 263,
+    width: "100%",
+    height: 750,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    backgroundColor: "#fff",
+  },
+  avatar: {
+    position: "absolute",
+    width: 120,
+    height: 120,
+    backgroundColor: "#F6F6F6",
+    borderRadius: 16,
+  },
+  addBtn: {
+    position: "absolute",
+    width: 25,
+    height: 25,
+    left: 107,
+    bottom: 14,
+  },
+  title: {
+    fontFamily: "RobotoBold",
+    lineHeight: 35,
+    fontSize: 30,
+    textAlign: "center",
+    letterSpacing: 0.01,
+    marginTop: 92,
+  },
 });
